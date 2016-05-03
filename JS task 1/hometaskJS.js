ddfa9cb350780e2af27cd8ff350b4de57910c14c
Oleksandra Strilenko <aleksandra_str@yahoo.com>
2016-05-03 20:04:10 +0300

    Extra console.logs deleted

--- JS task 1/hometaskJS.js
+++ JS task 1/hometaskJS.js
@@ -1,13 +1,9 @@
 
 
 var InsertNum = prompt('Укажите число которое хотите возвести в степень:',100);
-// console.log('Пользователь ввел: ', InsertNum);
-// console.log('Пользователь ввел type: ', typeof InsertNum);
-
 
 var InsertExponent = prompt('Укажите степень:', 100);
-// console.log('Пользователь ввел: ', InsertExponent);
-// console.log('Пользователь ввел type: ', typeof InsertExponent);
+
 
 ExtentPow()
 
@@ -15,19 +11,13 @@ ExtentPow()
 function ExtentPow() {
 	var InsertNum2 = +InsertNum;
 	var InsertExponent2 = +InsertExponent;
-	console.log('После приведения в переменной записано ', InsertNum2);
-	console.log('Тип после приведения: ', typeof InsertNum2);
-	console.log('После приведения в переменной записано: ', InsertExponent2);
-	console.log('Тип после приведения: ', typeof InsertExponent2);
-	
+		
 	if ( isNaN(InsertNum2) || InsertNum2 == "undefined" || parseInt(InsertNum2) !== InsertNum2) {
 	alert('Вычисление не может быть выполнено без указания целого числа');
-	console.log('InsertNum2 = ', InsertNum2);
 	}
 
 	else if (isNaN(InsertExponent2) || InsertExponent2 == "undefined" || parseInt(InsertExponent2) !== InsertExponent2 ) {
 		alert('Вычисление не может быть выполнено без указания целой степени');
-		console.log('InsertExponent = ', InsertExponent2);
 	} 
 	
 
@@ -38,10 +28,7 @@ function ExtentPow() {
 		while (i <= ((InsertExponent2*-1)-2) ) {
 		var FinalResult = FinalResult*InsertNum2;
 		i++;
-		console.log('i = ', i);
-		console.log('FinalResult = ', FinalResult);
 		}
-
 		var FinalResult = 1 / FinalResult;
 		console.log('FinalResult для отрицательной степени = ', FinalResult);
 	} else {
@@ -51,8 +38,6 @@ function ExtentPow() {
 		while (i <= (InsertExponent2-2) ) {
 		var FinalResult = FinalResult*InsertNum2;
 		i++;
-		console.log('i = ', i);
-		console.log('FinalResult = ', FinalResult);
 		}
 	}
 
